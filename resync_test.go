a4c02c1b53c826182830a7fa7fecc2283eecf9b8
Andy Gorman <andyg@apacesystems.com>
2022-11-29 09:51:46 -0800

    More tests

--- resync_test.go
+++ resync_test.go
@@ -18,6 +18,7 @@ func TestResync(t *testing.T) {
 	config := &Config{
 		Retention:    Int(0),
 		SecondsField: Bool(true),
+		TimeLimit:    String("5s"),
 		Syncs: map[string]*Sync{
 			"test": {
 				Command:  String(fmt.Sprintf("-a ./testdata/a/ %s", dir)),
@@ -49,4 +50,48 @@ func TestResync(t *testing.T) {
 	b, err := os.ReadFile(filepath.Join(dir, "test"))
 	assert.Nil(t, err)
 	assert.Equal(t, b, []byte("Hello World"))
+
+	err = re.Dump()
+	assert.Error(t, err)
+}
+
+func TestStart(t *testing.T) {
+	dir, err := os.MkdirTemp("", "resync_test")
+	assert.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	config := &Config{
+		LogPath: String(dir),
+		LibPath: String(dir),
+		Syncs: map[string]*Sync{
+			"test": {
+				Command:  String(fmt.Sprintf("-a ./testdata/a/ %s", dir)),
+				Schedule: String("* * * * *"),
+			},
+		},
+	}
+	err = config.validate()
+	assert.Nil(t, err)
+
+	db, err := NewBoltDB(config)
+	assert.Nil(t, err)
+
+	logger := NewFSLogger(config)
+
+	mailer := NewMailer(config, db, logger)
+	assert.NotNil(t, mailer)
+
+	re := New(config, db, logger, mailer)
+	assert.NotNil(t, re)
+
+	err = re.Start()
+	assert.Nil(t, err)
+	err = re.Start()
+	assert.Nil(t, err)
+
+	re.Stop()
+	re.Stop()
+
+	err = re.Dump()
+	assert.Nil(t, err)
 }
