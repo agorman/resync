11c6c0a9aa5f9d0c4eb5e1f7812a2f1ee6a3f138
Andy Gorman <andyg@apacesystems.com>
2022-12-04 11:57:51 -0800

    Increasing test coverage

--- notifier_email_test.go
+++ notifier_email_test.go
@@ -7,6 +7,26 @@ import (
 )
 
 func TestMailer(t *testing.T) {
+	config, err := OpenConfig("./testdata/resync.yaml")
+	assert.Nil(t, err)
+
+	db, err := NewBoltDB(config)
+	assert.Nil(t, err)
+
+	logger := NewFSLogger(config)
+
+	emailNotifier := NewEmailNotifier(config, db, logger)
+	assert.NotNil(t, emailNotifier)
+
+	// Testing that mailing with retention = 0 is a noop
+	err = emailNotifier.Notify(NewStat("TEST", "Mon Jan 02 03:04:05 PM MST"))
+	assert.Error(t, err)
+
+	err = emailNotifier.NotifyHistory()
+	assert.Error(t, err)
+}
+
+func TestSkipMailer(t *testing.T) {
 	config := &Config{
 		Retention: Int(0),
 	}
--- vars_test.go
+++ vars_test.go
@@ -12,16 +12,19 @@ func TestVars(t *testing.T) {
 	assert.NotNil(t, strp)
 	assert.Equal(t, *strp, s)
 	assert.Equal(t, StringValue(strp), s)
+	assert.Equal(t, StringValue(nil), "")
 
 	i := 123
 	intp := Int(i)
 	assert.NotNil(t, intp)
 	assert.Equal(t, *intp, i)
 	assert.Equal(t, IntValue(intp), i)
+	assert.Equal(t, IntValue(nil), 0)
 
 	b := true
 	boolp := Bool(b)
 	assert.NotNil(t, boolp)
 	assert.Equal(t, *boolp, b)
 	assert.Equal(t, BoolValue(boolp), b)
+	assert.Equal(t, BoolValue(nil), false)
 }
